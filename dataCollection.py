be8cfab258453a6e33adcf905e574088c9cbbacc
Syed Asad Jamil <syedasad1410@gmail.com>
2024-05-09 21:35:43 +0500

    daya collected and save to folder

--- dataCollection.py
+++ dataCollection.py
@@ -2,15 +2,22 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
+import time
+import os
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 
 offset = 20
 imgSize = 300
+counter = 0
+
+folder = "Data/C"
+if not os.path.exists(folder):
+    os.makedirs(folder)
 
 while True:
-    ret, img = cap.read()
+    ret, img = cap.read
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
@@ -25,6 +32,7 @@ while True:
         if aspectRatio > 1:
             k = imgSize/h
             wCal = math.ceil(k*w)
+            # print("Width Cal:", wCal)
             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize-wCal)/2)
@@ -41,4 +49,18 @@ while True:
         cv2.imshow('ImageWhite', imgWhite)
 
     cv2.imshow('frame', img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    # if key == ord("s"):
+    #     counter += 1
+    #     cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+    #     print(counter)
+    if key == ord("s"):
+        counter += 1
+        timestamp = time.time()
+        file_path = os.path.join(folder, f'Image_{timestamp}.jpg')
+        if cv2.imwrite(file_path, imgWhite):
+            print(f"Image saved successfully: {folder}")
+        else:
+            print(f"Error saving image: {folder}")
+
+        print(counter)
